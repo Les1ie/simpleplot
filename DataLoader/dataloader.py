ef07134b434ae2b23751e483013113b08a761f8b
WuZhen <498721344@qq.com>
2020-10-18 21:30:55 +0800

    完成了文件读取边信息并画图

---
+++ DataLoader/dataloader.py
@@ -0,0 +1,35 @@
+import csv
+import pandas as pd
+import numpy as np
+
+
+class DataLoaderOption():
+    '''
+    '''
+    pass
+
+
+class DataLoader():
+
+    opt = DataLoaderOption()
+
+    def __init__(self, file_name: str) -> None:
+        assert file_name.split(".")[-1].strip() == "csv",\
+            "Only support csv file in current version."
+
+        self.data = pd.read_csv(file_name,)
+
+        self.data = self.data.astype(str)
+        self.data = self.data.where(self.data != 'nan', None)
+
+        # print(self.data.dtypes)
+        # print(self.data)
+
+    def tolist(self) -> list:
+        return self.data.values.tolist()
+
+
+def csv_to_list(file_name: str):
+    dl = DataLoader(file_name=file_name)
+
+    return dl.tolist()
